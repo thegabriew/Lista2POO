ca512cb0c1396286b21dab92261be83da4de067f
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-10-10 14:50:44 -0300

    Create quinta.java

---
+++ quinta.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class quinta {
+    
+    public static void main (String [] args){
+
+        Scanner ler = new Scanner(System.in);
+        int idade, meses, dias, anos;
+
+        System.out.println("Insira sua idade em: ");
+        System.out.print("Anos: ");
+        anos = ler.nextInt();
+        System.out.print("Meses: ");
+        meses = ler.nextInt();
+        System.out.print("Dias: ");
+        dias = ler.nextInt();
+
+    }
+}
